d52b840e8fabbd78eae1023f370f10b08c5be4e8
Akash Saraswat <AkashSaraswat243@gmail.com>
2024-07-08 22:13:24 +0530

    First commit

--- index.js
+++ index.js
@@ -23,11 +23,6 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
-// HOME ROUTE
-app.get('/', (req, res) => {
-    res.send("home is working");
-});
-
 // INDEX ROUTE
 app.get('/chats', async (req, res) => {
     let chats = await Chat.find();
