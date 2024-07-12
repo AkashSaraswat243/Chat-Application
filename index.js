3e95620348b5263aff7205c5080ae0df0ed77bcf
Akash Saraswat <AkashSaraswat243@gmail.com>
2024-07-12 12:20:25 +0530

    update index.js

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
+// HOME ROUTE
+app.get('/', (req, res) => {
+    res.send("Home page");
+});
+
 // INDEX ROUTE
 app.get('/chats', async (req, res) => {
     let chats = await Chat.find();
